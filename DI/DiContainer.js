1c053fef8eb6f8319ed229521471ae9cc1f6da04
= <=>
2016-09-26 14:11:17 +0800

    add DiContainer test

--- DI/DiContainer.js
+++ DI/DiContainer.js
@@ -1,3 +1,41 @@
-/**
- * Created by ljj on 2016/9/23.
- */
+DiContainer = function () {
+    this.registrations = [];
+
+};
+DiContainer.prototype.messages = {
+    registerRequiesArgs: 'The register function requires three arguments: ' +
+    'a string,an array of strings,and a function.'
+};
+
+DiContainer.prototype.register = function (name, dependencies, func) {
+    if (typeof name !== 'string'
+        || !Array.isArray(dependencies)
+        || typeof func !== 'function') {
+        throw new Error(this.messages.registerRequiesArgs);
+    }
+
+    for (let ix = 0; ix < dependencies.length; ix++) {
+        if (typeof dependencies[ix] !== 'string') {
+            throw new Error(this.messages.registerRequiesArgs);
+        }
+    }
+    this.registrations[name] =
+    {dependencies: dependencies, func: func};
+
+};
+
+DiContainer.prototype.get = function (name) {
+    let self = this;
+    let registration = this.registrations[name];
+    let dependencies = [];
+    if (registration === undefined) {
+        return undefined;
+    }
+    registration.dependencies.forEach(function (dependencyName) {
+        let dependency = self.get(dependencyName);
+        dependencies.push(dependencyName === undefined ? undefined : dependency);
+    });
+
+
+    return registration.func.apply(undefined, dependencies);
+};
--- DI/DiContainer.tests.js
+++ DI/DiContainer.tests.js
@@ -1,3 +1,75 @@
-/**
- * Created by ljj on 2016/9/23.
- */
+describe('DiContainer', function () {
+    var container;
+    beforeEach(function () {
+        container = new DiContainer();
+    });
+
+    describe('register(name,dependencies,func)', function () {
+        it('should throws exception if any argument is missing or the wrong type',
+            function () {
+                var badArgs = [[],
+                    ['Name'],
+                    ['Name', ['D1', 'D2']],
+                    ['Name', function () {
+                    }],
+                    [1, ['a', 'b'], function () {
+                    }],
+                    ['Name', [1, 2], function () {
+                    }],
+                    ['Name', ['a', 'b'], 'should be a function']
+                ];
+                badArgs.forEach(function (args) {
+                    expect(function () {
+                        container.register.apply(container, args);
+                    }).toThrowError(container.messages.registerRequiesArgs);
+
+                });
+
+            });
+
+    });
+
+    describe('get(name)', function () {
+        it('returns undefinded if name was not registered', function () {
+            expect(container.get('notDefined')).toBeUndefined();
+        });
+
+        it('return the resule of executing the registered function', function () {
+            var name = 'MyName';
+            var returnFromReisteredFunction = "something";
+            container.register(name, [], function () {
+                return returnFromReisteredFunction;
+            });
+            expect(container.get(name)).toBe(returnFromReisteredFunction);
+        });
+
+        it('supplies dependencies to the registered function', function () {
+            var main = 'main',
+                mainFunc,
+                dep1 = 'dep1',
+                dep2 = 'dep2';
+            container.register(main, [dep1, dep2], function (dep1Func, dep2Func) {
+                return function () {
+                    return dep1Func() + dep2Func();
+                };
+            });
+            container.register(dep1, [], function () {
+                return function () {
+                    return 1;
+                };
+            });
+            container.register(dep2, [], function () {
+                return function () {
+                    return 2;
+                };
+            });
+
+            mainFunc = container.get(main);
+
+            expect(mainFunc()).toBe(3);
+
+        });
+    });
+
+
+});
